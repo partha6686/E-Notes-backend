916d2dff56612aec2b99fb585f4ad32679a79d2b
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2022-01-26 00:23:36 +0530

    Sort blogs

--- routes/notes.js
+++ routes/notes.js
@@ -6,10 +6,10 @@ const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 //* Get all Public notes GET: '/api/notes/' Login NOT Required
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     try {
-        const notes = await Notes.find({status: 'public'});
-        res.json(notes); 
+        const notes = await Notes.find({ status: 'public' }).sort({ _id: -1 });
+        res.json(notes);
     } catch (error) {
         //* Send Internal Server Error
         console.error(error.message);
@@ -18,10 +18,10 @@ router.get('/', async (req,res)=>{
 });
 
 //* Get all notes of a User GET: '/api/notes/user' Login Required
-router.get('/user', fetchuser, async (req,res)=>{
+router.get('/user', fetchuser, async (req, res) => {
     try {
-        const notes = await Notes.find({user: req.user.id})
-        res.json(notes); 
+        const notes = await Notes.find({ user: req.user.id }).sort({ _id: -1 });
+        res.json(notes);
     } catch (error) {
         //* Send Internal Server Error
         console.error(error.message);
@@ -35,9 +35,9 @@ router.post('/addnote', fetchuser, [
     body('title', 'Enter a Valid title').isLength({ min: 3 }),
     body('description', 'Description must have atleast 5 charecters').isLength({ min: 5 }),
     body('status', 'Invalid Status').isIn(['private', 'public'])
-], async (req,res)=>{
+], async (req, res) => {
     try {
-        const {title, description, tag, status} = req.body;
+        const { title, description, tag, status } = req.body;
         //* check errors and send Bad requests 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -61,27 +61,27 @@ router.post('/addnote', fetchuser, [
 })
 
 //* Update a note PUT: '/api/notes/updatenote' Login Required
-router.put('/updatenote/:id', fetchuser, async (req,res)=>{
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
     try {
-        const {title, description, tag, status} = req.body;
+        const { title, description, tag, status } = req.body;
         //* Create a new note object with the feilds which should be updated
         const updatedNote = {};
-        if(title){updatedNote.title = title}
-        if(description){updatedNote.description = description}
-        if(tag){updatedNote.tag = tag}
-        if(status){updatedNote.status = status}
+        if (title) { updatedNote.title = title }
+        if (description) { updatedNote.description = description }
+        if (tag) { updatedNote.tag = tag }
+        if (status) { updatedNote.status = status }
 
         //* Find the note and update it if it belongs to the user
         let note = await Notes.findById(req.params.id);
-        if(!note){
-            return res.status(404).json({error: "Note not Found"});
+        if (!note) {
+            return res.status(404).json({ error: "Note not Found" });
         }
-        if(note.user.toString() !== req.user.id){
-            return res.status(401).json({error: "Note not Found"});
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).json({ error: "Note not Found" });
         }
 
-        note = await Notes.findByIdAndUpdate(req.params.id, updatedNote, {new:true});
-        res.json({note});
+        note = await Notes.findByIdAndUpdate(req.params.id, updatedNote, { new: true });
+        res.json({ note });
 
     } catch (error) {
         //* Send Internal Server Error
@@ -91,19 +91,19 @@ router.put('/updatenote/:id', fetchuser, async (req,res)=>{
 })
 
 //* Delete a note DELETE: '/api/notes/deletenote' Login Required
-router.delete('/deletenote/:id', fetchuser, async (req,res)=>{
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
     try {
         //* Find the note and delete it if it belongs to the user
         let note = await Notes.findById(req.params.id);
-        if(!note){
-            return res.status(404).json({error: "Note not Found"});
+        if (!note) {
+            return res.status(404).json({ error: "Note not Found" });
         }
-        if(note.user.toString() !== req.user.id){
-            return res.status(401).json({error: "Note not Found"});
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).json({ error: "Note not Found" });
         }
 
         note = await Notes.findByIdAndDelete(req.params.id);
-        res.json({"Success": "Note has been deleted", "note": note});
+        res.json({ "Success": "Note has been deleted", "note": note });
 
     } catch (error) {
         //* Send Internal Server Error
